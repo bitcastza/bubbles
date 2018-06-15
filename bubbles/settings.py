82e2f90e923781e33ca08a9e829b6fae06025fb6
Kyle Robbertze <krobbertze@gmail.com>
2018-06-15 18:31:03 +0200

    set up timezone

--- bubbles/settings.py
+++ bubbles/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-za'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Johannesburg'
 
 USE_I18N = True
 
