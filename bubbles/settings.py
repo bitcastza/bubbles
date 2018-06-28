635fd2ffd232bb6c7975e247c1fc9c106079a304
Kyle Robbertze <krobbertze@gmail.com>
2018-06-29 00:53:59 +0200

    Enable sign in using Sakai
    Fixes #1

--- bubbles/settings.py
+++ bubbles/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bubbles',
+    'bubbles.registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sakaiauthenticator',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'sakaiauthenticator.sakaiauthenticator.SakaiAuthenticatorBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
@@ -113,8 +121,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+SAKAI_URL = 'https://vula.uct.ac.za'
+USE_SAKAI_SITE = True
+SAKAI_SITE_URL = '/site/d6d8ab20-ffa4-4047-b1fd-e6d98847a949'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login
+LOGIN_REDIRECT_URL = '/'
