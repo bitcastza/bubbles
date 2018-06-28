80200dc8e73aa07a5c8c35fa9a5e10b5116e71f0
Kyle Robbertze <krobbertze@gmail.com>
2018-06-29 00:59:10 +0200

    re-order installed apps

--- bubbles/settings.py
+++ bubbles/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'bubbles',
     'bubbles.registration',
+    'sakaiauthenticator',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sakaiauthenticator',
 ]
 
 MIDDLEWARE = [
