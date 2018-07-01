3eaf114ce1365c56fb10af72428d46a8f2d07730
Kyle Robbertze <krobbertze@gmail.com>
2018-07-01 22:42:49 +0200

    Add Licence headers

--- bubbles/inventory/urls.py
+++ bubbles/inventory/urls.py
@@ -1,6 +1,5 @@
 ###########################################################################
-# Bubbles is Copyright (C) 2018 Kyle Robbertze
-# <krobbertze@gmail.com>
+# Bubbles is Copyright (C) 2018 Kyle Robbertze <krobbertze@gmail.com>
 #
 # Bubbles is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
