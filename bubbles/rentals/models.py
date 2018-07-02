6312e4b6e4ad3dfe23c9197ebe77965842cd4887
Kyle Robbertze <krobbertze@gmail.com>
2018-07-03 01:39:38 +0200

    add rental models

--- bubbles/rentals/models.py
+++ bubbles/rentals/models.py
@@ -13,6 +13,25 @@
 # You should have received a copy of the GNU General Public License
 # along with Bubbles. If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
+from django.contrib.auth.models import User
 from django.db import models
+from bubbles.inventory.models import *
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Rental(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    approved_by = models.ForeignKey(User,
+                                    on_delete=models.CASCADE,
+                                    limit_choices_to={'is_staff': True},
+                                    related_name='approved_rentals_set',
+                                    null=True, blank=True)
+    start_date = models.DateField(_('Start date'))
+    period = models.DurationField(_('Rental duration'))
+    deposit = models.IntegerField(_('Deposit'))
+    notes = models.TextField(_('Notes'))
+    deposit_returned = models.BooleanField(_('Deposit returned'), default=True)
+
+class RentalItem(models.Model):
+    rental = models.ForeignKey(Rental, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    cost = models.IntegerField(_('Cost'), default=0)
