7d2e11cfc48a24e979282b02ef0f6aba7a950295
Kyle Robbertze <krobbertze@gmail.com>
2018-07-03 22:13:30 +0200

    support custom user models

--- bubbles/rentals/models.py
+++ bubbles/rentals/models.py
@@ -13,23 +13,27 @@
 # You should have received a copy of the GNU General Public License
 # along with Bubbles. If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 from bubbles.inventory.models import *
 from django.utils.translation import gettext_lazy as _
 
 class Rental(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    approved_by = models.ForeignKey(User,
-                                    on_delete=models.CASCADE,
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    approved_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                    on_delete=models.SET_NULL,
                                     limit_choices_to={'is_staff': True},
                                     related_name='approved_rentals_set',
                                     null=True, blank=True)
     start_date = models.DateField(_('Start date'))
     period = models.DurationField(_('Rental duration'))
     deposit = models.IntegerField(_('Deposit'))
-    notes = models.TextField(_('Notes'))
+    notes = models.TextField(_('Notes'), null=True, blank=True)
     deposit_returned = models.BooleanField(_('Deposit returned'), default=True)
+
+    def get_end_date(self):
+        return self.start_date + self.period
 
 class RentalItem(models.Model):
     rental = models.ForeignKey(Rental, on_delete=models.CASCADE)
