1e6f806a52d0fc16b4bb67b5bfd9688f8d3fd429
Kyle Robbertze <krobbertze@gmail.com>
2018-07-10 18:44:28 +0200

    test logic correctly

--- bubbles/inventory/tests.py
+++ bubbles/inventory/tests.py
@@ -22,7 +22,7 @@ from .models import Item, BCD, Cylinder, Regulator
 class BCDTests(TestCase):
     def test_next_service(self):
         last_service = datetime.date(year=2018, month=7, day=2)
-        period = datetime.timedelta(weeks=54)
+        next_service = datetime.date(year=2019, month=7, day=1)
         bcd = BCD.objects.create(number='1',
                                  manufacturer='test',
                                  date_of_purchase=last_service,
@@ -30,12 +30,12 @@ class BCDTests(TestCase):
                                  description='BCD',
                                  last_service=last_service,
                                  size=BCD.SMALL)
-        self.assertEqual(bcd.next_service, last_service + period)
+        self.assertEqual(bcd.next_service, next_service)
 
 class CylinderTests(TestCase):
     def test_next_service(self):
         last_viz = datetime.date(year=2018, month=7, day=2)
-        period = datetime.timedelta(weeks=54)
+        next_service = datetime.date(year=2019, month=7, day=1)
         cylinder = Cylinder.objects.create(number='1',
                                  manufacturer='test',
                                  date_of_purchase=last_viz,
@@ -46,19 +46,19 @@ class CylinderTests(TestCase):
                                  capacity=12,
                                  last_viz=last_viz,
                                  last_hydro=last_viz,
-                                 viz_period=datetime.timedelta(weeks=54),
+                                 viz_period=datetime.timedelta(weeks=52),
                                  hydro_period=datetime.timedelta(weeks=108))
 
-        self.assertEqual(cylinder.next_service, last_viz + period)
+        self.assertEqual(cylinder.next_service, next_service)
 
 class BCDTests(TestCase):
     def test_next_service(self):
         last_service = datetime.date(year=2018, month=7, day=2)
-        period = datetime.timedelta(weeks=54)
+        next_service = datetime.date(year=2019, month=7, day=1)
         regulator = Regulator.objects.create(number='1',
                                  manufacturer='test',
                                  date_of_purchase=last_service,
                                  state=Item.AVAILABLE,
                                  description='Regulator',
                                  last_service=last_service)
-        self.assertEqual(regulator.next_service, last_service + period)
+        self.assertEqual(regulator.next_service, next_service)
