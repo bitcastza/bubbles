9c7fcb0dbe52777e548abc42d02152a6bd803d54
Kyle Robbertze <krobbertze@gmail.com>
2019-04-24 00:28:24 +0200

    update Item and Rental if RentalItem changes

--- bubbles/rentals/signals.py
+++ bubbles/rentals/signals.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Bubbles. If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 from .models import RentalItem, Rental, Weight
 from bubbles.inventory.models import Item
@@ -28,3 +28,22 @@ def handle_delete_rental(sender, instance, using, **kwargs):
     total_weight = Weight.objects.first()
     total_weight.available_weight = total_weight.available_weight + instance.weight
     total_weight.save()
+
+@receiver(post_save, sender=RentalItem)
+def handle_edit_rentalitem(sender, instance, created, raw, using, **kwargs):
+    if not raw:
+        rental = instance.rental
+        # Update rental if all rental items are returned
+        if not rental.rentalitem_set.filter(returned=False).exists():
+            rental.state = Rental.RETURNED
+            rental.save()
+        else:
+            rental.state = Rental.RENTED
+            rental.save()
+
+        # Update item if rental item changes
+        if instance.returned:
+            instance.item.state = Item.AVAILABLE
+        else:
+            instance.item.state = Item.IN_USE
+        instance.item.save()
